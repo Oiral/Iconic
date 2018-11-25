5571eebb6470a3d63ae817d1c2aefbf12588308b
Hamish <hamish@pageland.co.nz>
2018-11-26 11:44:46 +1300

    Added in main menu

---
+++ Assets/Scripts/Managers/PauseScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Events;
+
+public class PauseScript : MonoBehaviour {
+
+    public static UnityEvent OnPauseEvent = new UnityEvent();
+
+    public static bool paused = false;
+
+    public GameObject pauseMenu;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause()
+    {
+        paused = !paused;
+        OnPauseEvent.Invoke();
+        pauseMenu.SetActive(paused);
+    }
+}
