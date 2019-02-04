1ff6c6ffb1dfb74b560004fa2b7be561cdb42bdd
Hamish <hamish@pageland.co.nz>
2019-02-04 22:07:55 +1300

    Added in primitive saving

---
+++ Assets/Scripts/Saving/SaveSystem.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveSystem {
+
+    static string ScorePath = "/score.savedata";
+
+	public static void SaveScore(int score)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + ScorePath;
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        ScoreData data = new ScoreData();
+        data.highScores.Add(score);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static void SaveScore(ScoreData data)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + ScorePath;
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static ScoreData LoadScores()
+    {
+        string path = Application.persistentDataPath + ScorePath;
+        if(File.Exists(path)){
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            ScoreData data = (ScoreData)formatter.Deserialize(stream);
+
+            stream.Close();
+             
+            return data;
+            
+        }
+        else
+        {
+            Debug.LogError("Save file not found in " + path);
+            return null;
+        }
+    }
+
+    public static void ClearScores() {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + ScorePath;
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        ScoreData data = new ScoreData();
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+}
---
+++ Assets/TestingScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingScript : MonoBehaviour {
+
+    public int testScore = 100;
+
+    private void Start()
+    {
+        SaveSystem.ClearScores();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            ScoreData data = SaveSystem.LoadScores();
+            if (data.highScores != null)
+            {
+                data.highScores.Add(testScore);
+                SaveSystem.SaveScore(data);
+            }
+            else
+            {
+                Debug.Log("Creating new save");
+                SaveSystem.SaveScore(testScore);
+            }
+            DebugList(SaveSystem.LoadScores().highScores);
+
+        }
+	}
+
+    private void DebugList(List<int> list)
+    {
+        string debugString = "";
+        for (int i = 0; i < list.Count; i++)
+        {
+            debugString += list[i].ToString() + " | ";
+        }
+        Debug.Log(debugString);
+    }
+}
