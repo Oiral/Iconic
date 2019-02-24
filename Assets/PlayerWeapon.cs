ea0e55da2e8d7a45fb3140d75157efa00aa110ad
Hamish Yoobee <hamish@pageland.co.nz>
2019-02-25 11:47:21 +1300

    update 0.2 and started on new weapon system

---
+++ Assets/PlayerWeapon.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum weaponType {normal, explosive, homing, following};
+
+public class PlayerWeapon : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+
+    public int FireRate;
+    public int MultiShot;
+        
+}
